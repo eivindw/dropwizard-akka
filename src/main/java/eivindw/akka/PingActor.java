f0b79862fe7994b1addf6e320c4d57d3e2a7f133
eivindw <eivindw@gmail.com>
2014-12-02 22:31:47 +0100

    Look at aroundReceive.

--- src/main/java/eivindw/akka/PingActor.java
+++ src/main/java/eivindw/akka/PingActor.java
@@ -4,10 +4,19 @@ import akka.actor.AbstractActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import akka.japi.pf.ReceiveBuilder;
+import scala.PartialFunction;
+import scala.runtime.BoxedUnit;
 
 public class PingActor extends AbstractActor {
 
    private final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
+   @Override
+   public void aroundReceive(PartialFunction<Object, BoxedUnit> receive, Object msg) {
+      log.info("Before receive? {}", msg);
+      super.aroundReceive(receive, msg);
+      log.info("After receive? {}", msg);
+   }
 
    public PingActor() {
       receive(
