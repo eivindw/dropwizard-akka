53ae1b55ce1cef631cc1c5c946225fb08f037254
eivindw <eivindw@gmail.com>
2014-12-22 08:01:39 +0100

    Add async REST example.

--- src/main/java/eivindw/api/TestResource.java
+++ src/main/java/eivindw/api/TestResource.java
@@ -1,6 +1,8 @@
 package eivindw.api;
 
 import akka.actor.ActorRef;
+import akka.dispatch.ExecutionContexts;
+import akka.dispatch.OnSuccess;
 import akka.pattern.Patterns;
 import com.google.common.collect.ImmutableMap;
 import eivindw.akka.Message;
@@ -8,10 +10,13 @@ import eivindw.service.YoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.concurrent.Await;
+import scala.concurrent.ExecutionContextExecutorService;
 import scala.concurrent.Future;
 import scala.concurrent.duration.Duration;
 
 import javax.ws.rs.*;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import java.util.Map;
 import java.util.concurrent.Executors;
 
@@ -23,9 +28,11 @@ public class TestResource {
    private final Logger log = LoggerFactory.getLogger(TestResource.class);
 
    private final ActorRef pinger;
+   private final ExecutionContextExecutorService execCtx;
 
    public TestResource(ActorRef pinger) {
       this.pinger = pinger;
+      execCtx = ExecutionContexts.fromExecutorService(Executors.newFixedThreadPool(2));
    }
 
    @GET
@@ -57,6 +64,28 @@ public class TestResource {
       return ImmutableMap.of(
          "name", "Other",
          "age", 27
+      );
+   }
+
+   @GET
+   @Path("async/{user}")
+   public void async(@Suspended AsyncResponse response, @PathParam("user") String user) {
+      log.info("Async request start");
+
+      final Future<Object> pong =
+         Patterns.ask(pinger, new Message<>(user, "yoyo from " + user), 100);
+
+      pong.onSuccess(
+         new OnSuccess<Object>() {
+            @Override
+            public void onSuccess(Object result) throws Throwable {
+               log.info("Async returning {}", result);
+               response.resume(ImmutableMap.of(
+                  "result", result
+               ));
+            }
+         },
+         execCtx
       );
    }
 }
